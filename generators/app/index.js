c7ca7d1d37d9c552fa6db27e577283bc1dafa163
AkashGutha <akash.gutha@outlook.com>
2018-02-12 00:27:39 +0530

    prettier fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,16 +6,20 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the bee\'s knees ' + chalk.red('generator-stencil') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        "Welcome to the bee's knees " + chalk.red('generator-stencil') + ' generator!'
+      )
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
