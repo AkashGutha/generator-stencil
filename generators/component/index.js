78cad055ce3b16b7b4bb0c7617e5b902ab45c461
AkashGutha <akash.gutha@outlook.com>
2018-02-12 09:39:10 +0530

    added sub generator

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,26 +1,17 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const yosay = require('yosay');
 const changeCase = require('change-case');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to ' + chalk.red('stencil') + ' generator!'));
 
     const prompts = [
       {
         type: 'input',
-        name: 'appName',
-        message: "What is your app's name?",
-        default: 'app'
-      },
-      {
-        type: 'input',
         name: 'componentName',
-        message: 'Would you like your component name to be?',
-        default: 'my-component'
+        message: "What is your component's name?",
+        default: 'MyComponent'
       }
     ];
 
@@ -33,12 +24,6 @@ module.exports = class extends Generator {
   writing() {
     const componentName = this.props.componentName;
     this.props.paramCaseComponentName = changeCase.paramCase(componentName);
-
-    this.fs.copyTpl(
-      this.templatePath(`../app/**`),
-      this.destinationPath(`/`),
-      this.props
-    );
 
     this.fs.copyTpl(
       this.templatePath(`_component.tsx`),
