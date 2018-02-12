5a71957907ddeda556fe2c6765c6421f326623ce
AkashGutha <akash.gutha@outlook.com>
2018-02-12 22:04:08 +0530

    FIX: scss and css extensions

--- component/templates/_component.tsx
+++ component/templates/_component.tsx
@@ -2,10 +2,9 @@ import { Component } from '@stencil/core';
 
 @Component({
   tag: '<%= paramCaseComponentName %>',
-  styleUrl: '<%= paramCaseComponentName %>.css'
+  styleUrl: '<%= paramCaseComponentName %>.<% if(sassSupport) { %>scss<% } %><% if(!sassSupport) { %>css<% } %>'
 })
 export class <%=componentName%> {
-
   render() {
     return (
       <div>
