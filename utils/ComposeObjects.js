2d9f974d7f0db292014291443d5ede9244b8dd9c
AkashGutha <akash.gutha@outlook.com>
2018-02-13 21:49:24 +0530

    REFACTOR: compose multiple objs

--- utils/ComposeObjects.js
+++ utils/ComposeObjects.js
@@ -1,3 +1,10 @@
-module.exports = function (object1, object2) {
-  return { ...object1, ...object2 };
+function composeObjs(...args) {
+  const arguments = args;
+  let obj = {};
+  for (const argument of arguments) {
+    obj = { ...obj, ...argument }
+  }
+  return obj;
 };
+
+module.exports = composeObjs;
