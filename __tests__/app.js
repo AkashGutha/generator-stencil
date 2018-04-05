2cbb03edde8a765520ade674fac4b36b1e30e368
AkashGutha <akash.gutha@outlook.com>
2018-04-05 19:30:18 +0530

    run prettier

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,12 @@ const helpers = require('yeoman-test');
 
 describe('generator-stencil:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['dummyfile.txt']);
   });
 });
